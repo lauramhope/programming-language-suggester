965af5b9e1891890d4fa74f0d161998f8b9a8678
Laura Hope <ssteamlaura@gmail.com>
2023-03-24 09:27:33 -0700

    Add script.js information including declaring variables and setting up function.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+const firstName = document.getElementById("firstName").value;
+
+document.querySelector("span#firstName").innerText = firstName; 
+
+function handleRadio(event) {
+  event.preventDefault();
+  const radioBev = document.querySelector("input[name='bev']").value;
+  const radioVacation = document.querySelector("input[name='vacation']").value;
+  const radioFlavor = document.querySelector("input[name='flavor']").value;
+  const radioPet = document.querySelector("input[name='pet']").value;
+  const radioColor = document.querySelector("input[name='color']").value;
+
+
+  let result;
+  if ()
+}
